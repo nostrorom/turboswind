7f3b3200b6bee1e12c8aaa0e134211c29bed2864
Nostro <49858211+Nostrorom@users.noreply.github.com>
2023-06-23 11:49:17 +0200

    svelte4

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -1,1 +1,12 @@
-export { default as default } from '@turboswind/playwright';
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+	webServer: {
+		command: 'npm run build && npm run preview',
+		port: 4173
+	},
+	testDir: 'tests',
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+};
+
+export default config;
